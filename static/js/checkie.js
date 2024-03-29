71e7fb345f8e31242863208a26e0a9a94fa1dcc7
wifi_left <ysy1679682327@outlook.com>
2024-03-29 20:17:18 +0800

    UI FIX

--- static/js/checkie.js
+++ static/js/checkie.js
@@ -1,3 +1,14 @@
 try { eval("()=>{}") } catch (e) { location.href = "./updatebrowser.html" }
 try { eval("let a = \"111\"") } catch (e) { location.href = "./updatebrowser.html" }
 try { eval("var a = `111`") } catch (e) { location.href = "./updatebrowser.html" }
+// 兼容老版本 Chrome
+if (String.prototype.replaceAll == undefined) {
+    String.prototype.replaceAll = function (searchString, replaceString, ignoreCase) {
+        if (RegExp.prototype.isPrototypeOf(searchString)) {
+            return this.replace(searchString, replaceString);
+        } else {
+            return this.replace(new RegExp(searchString, (ignoreCase ? "gmi" : "gm")), replaceString);
+        }
+    }
+    console.log("已定义自定义 replaceAll")
+}
